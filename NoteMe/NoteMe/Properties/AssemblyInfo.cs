8cef85e8fb7b0b84e88c44a4d9d39749bdb80572
Ayrton E <ayrton.94.e@gmail.com>
2022-04-10 21:59:03 -0500

    Icons and style.

--- NoteMe/NoteMe/Properties/AssemblyInfo.cs
+++ NoteMe/NoteMe/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FontAwesome6Brands-Regular-400.otf", Alias = "FABrands")]
+[assembly: ExportFont("FontAwesome6Free-Regular-400.otf", Alias = "FARegular")]
+[assembly: ExportFont("FontAwesome6Free-Solid-900.otf", Alias = "FASolid")]
